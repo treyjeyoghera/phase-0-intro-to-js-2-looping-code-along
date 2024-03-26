b3e89da722f4e9575e58c0d1da741b76280522e1
treyjeyoghera <treyjeyogheraeugene@gmail.com>
2024-03-26 20:05:37 +0300

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+// Code your solutions in this fil
+function countDown( startnum ) {
+    while ( startnum > 0 ) {
+      console.log( startnum );
+      startnum -= 1;
+    }
+    console.log( startnum );
+  }
+  function writeCards(name, surprise) {
+    return [
+        `Thank you, ${name[0]}, for the wonderful ${surprise} gift!`,
+        `Thank you, ${name[1]}, for the wonderful ${surprise} gift!`,
+        `Thank you, ${name[2]}, for the wonderful ${surprise} gift!`
+    ];
+}
+
+const messages = writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+console.log(messages);
